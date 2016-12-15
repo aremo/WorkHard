32fc6927ab523b892ac4f3c2cefb3bcd531efdc2
Arian Kompani Zare <arian.k.zare@gmail.com>
2016-12-15 21:16:43 +0100

    pierwszy commit

---
+++ src/WorkHard.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aremo on 12/15/2016.
+ */
+public class WorkHard {
+
+    public static void main(String[] args) {
+        System.out.print("Uczę\nsię\nprogramować");
+    }
+
+}
